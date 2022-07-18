aea5879d3b6474f47fa4be2fcc0e0c5e35758f38
vero-sari <veronica.sari.est@tecazuay.edu.ec>
2022-07-18 11:25:58 -0500

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,9 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'APECS';
 
-  constructor(private router:Router) { }
+  constructor(private router:Router) { 
+    
+  }
 
   gestionCliente(){
     this.router.navigate(['/gestion-clientes']);
