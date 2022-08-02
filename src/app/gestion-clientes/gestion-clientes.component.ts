82f971e61daa62ea93513bdf310cf250a198545b
vero-sari <veronica.sari.est@tecazuay.edu.ec>
2022-08-02 11:27:22 -0500

    9

--- src/app/gestion-clientes/gestion-clientes.component.ts
+++ src/app/gestion-clientes/gestion-clientes.component.ts
@@ -1,37 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Cliente } from '../models/Cliente';
-
-
 import { ClienteService } from '../service/cliente.service';
 
 @Component({
   selector: 'app-gestion-clientes',
   templateUrl: './gestion-clientes.component.html',
   styleUrls: ['./gestion-clientes.component.css']
-})
+})  
 export class GestionClientesComponent implements OnInit {
-formCliente:FormGroup;
-clientes: Cliente[];
-cliente: Cliente=new Cliente();
+
     
   constructor(
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private clienteService:ClienteService
+    
    ) { }
 
   ngOnInit(): void {
-    this.cliente=new Cliente();
-   this.listaClientes();
+  
   }
   listaClientes(){
-    this.clienteService.getClientes().subscribe((resp: any)=>{
-      console.log(resp.data)
-      this.cliente = resp.data
-    }
-    )
+   
+   
   }
 
 
